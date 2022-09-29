b3c2c6b9fecd8c5fb03d9877c30976e850503582
bamsi <hibamsi@gmail.com>
2022-09-29 18:10:03 +0300

    add transactions page

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -2,4 +2,20 @@ class ExpendituresController < ApplicationController
   def index
     @category = Category.find(params[:category_id])
   end
+
+  def new
+    @expenditure = Expenditure.new
+    @category_id = params[:category_id]
+  end
+
+  def create
+    @expenditure = Expenditure.new(expenditure_params)
+    @expenditure.author = current_user
+    @expenditure.category_id = params[:category_id]
+    redirect_to category_expenditures_path(category_id: params[:category_id]), notice: 'You have successfully added transaction' if @expenditure.save
+  end
+
+  def expenditure_params
+    params.require(:expenditure).permit(:name, :amount)
+  end
 end
